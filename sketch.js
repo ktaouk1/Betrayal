156465912fe8534848c14ad63583c11d98d27c54
ktaouk1 <ktaouk_97@hotmail.com>
2018-11-29 21:26:18 +1100

    characters.json added

--- sketch.js
+++ sketch.js
@@ -1,5 +1,22 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(800, 800);
+	background(51);
+
+	// Load character data
+	var characters = (function() {
+		var json = null;
+		$.ajax({
+			'async' : false,
+			'global' : false,
+			'url' : "/characters.json",
+			'dataType' : "json",
+			'success' : function (data) {
+				json = data;
+			}
+		});
+		return json;
+	})();
+	console.log(characters);
 }
 
 function draw() {
