2536dd9eb061d15755826dd83d963868ed0dd1e5
ktaouk1 <ktaouk_97@hotmail.com>
2018-12-31 07:59:01 +1100

    Added content, styling, light/dark theme

--- Javascript/sketch.js
+++ Javascript/sketch.js
@@ -6,8 +6,14 @@
 
 var characters = [];
 var tiles = [];
+var players = [];
 
 function setup() {
+
+
+	/* INITIALISE DATA AND VARIABLES */
+
+
 	createCanvas(800, 800);
 	background(51);
 	//Basic alignment grid
@@ -51,6 +57,46 @@ function setup() {
 	})();
 	tiles = Object.entries(tile);
 
+	//Light and dark theme buttons
+	var lighttheme = document.getElementById("light").addEventListener("click", function() {
+		var info = document.getElementById("info");
+		var actions = document.getElementById("actions");
+		var title = document.getElementById("title");
+		var body = document.getElementById("body");
+		info.style.backgroundColor = "#eee";
+		info.style.borderColor = "rgb(51, 51, 51)";
+		info.style.color = "rgb(51, 51, 51)";
+		actions.style.backgroundColor = "#eee";
+		actions.style.borderColor = "rgb(51, 51, 51)";
+		actions.style.color = "rgb(51, 51, 51)";
+		title.style.color = "rgb(51, 51, 51)";
+		body.style.backgroundColor = "white";
+	});
+	var darktheme = document.getElementById("dark").addEventListener("click", function() {
+		var info = document.getElementById("info");
+		var actions = document.getElementById("actions");
+		var title = document.getElementById("title");
+		var body = document.getElementById("body");
+		info.style.backgroundColor = "rgb(27, 29, 31)";
+		info.style.borderColor = "rgb(86, 85, 83)";
+		info.style.color = "rgb(230, 226, 219)";
+		actions.style.backgroundColor = "rgb(27, 29, 31)";
+		actions.style.borderColor = "rgb(86, 85, 83)";
+		actions.style.color = "rgb(230, 226, 219)";
+		title.style.color = "rgb(230, 226, 219)";
+		body.style.backgroundColor = "rgb(24, 25, 26)";
+	});
+
+
+	/* VISUALISE BOARD TILES
+		This includes:
+			> Checking the amount of free doors a tile has
+			> Selecting a new tile from the stack
+			> Drawing the tile at a random free position
+	*/
+
+
+	// TEMPORARY TILE CODE
 	var landingZone;
 	for (var i = 0; i < tiles.length; i++) {
 		if (tiles[i][0] == "Entrance_Hall") {
@@ -63,6 +109,84 @@ function setup() {
 	textSize(8.5);
 	fill(255);
 	text(landingZone[0].replace(/\_/g, " "), width/2-25, height/2-15);
+
+
+	/* GAMEPLAY */
+
+
+	// Get 4 random players for the game..
+	var temp = characters;
+	for (var i = 0; i < 4; i++) {
+		var index = Math.floor(Math.random() * temp.length);
+		var player = temp[index];
+		temp.splice(index, 1);
+		players.push(player);
+	}
+
+	//para.textContent += ;
+	//Introductory message
+	var para = document.getElementById("news");
+	para.textContent += "Welcome to Betrayal at House on the Hill! These are our players:\r\n\r\n";
+	//Display 4 random playable characters
+	for (var i = 0; i < players.length; i++) {
+		console.log(players[i]);
+		para.textContent += " > " + players[i][0].replace(/\_/g, " ") + ":\n";
+		para.textContent += "Speed: " + players[i][1].speed[players[i][1].speedIndex] + "\r\n";
+		para.textContent += "Might: " + players[i][1].might[players[i][1].mightIndex] + "\r\n";
+		para.textContent += "Sanity: " + players[i][1].sanity[players[i][1].sanityIndex] + "\r\n";
+		para.textContent += "Knowledge: " + players[i][1].knowledge[players[i][1].knowledgeIndex] + "\r\n\r\n";
+	}
+	para.textContent += "All players are currently in the Entrance Hall. Who do you want to play as?\r\n";
+	//Buttons for choosing player
+	var actions = document.getElementById("actions");
+	for (var i = 0; i < players.length; i++) {
+		var btn = document.createElement("button");
+		btn.setAttribute("id", players[i][0]);
+		btn.setAttribute("class", "players");
+		btn.innerHTML = "Play as " + players[i][0].replace(/\_/g, " ");
+		btn.addEventListener("click", function() {
+			para.textContent += "You chose " + this.id.replace(/\_/g, " ") + ".\r\n\r\n";
+			//Clear actions section, display stats for chosen player.
+			actions.innerHTML = "";
+			actions.textContent += this.id.replace(/\_/g, " ") + "\r\n\r\n";
+			var speed = "Speed: ";
+			var might = "Might: ";
+			var sanity = "Sanity: ";
+			var knowledge = "Knowledge: ";
+			var main;
+			//Get the selected character data
+			for (var i = 0; i < players.length; i++) {
+				if (this.id == players[i][0]) {
+					main = players[i];
+				}
+			}
+			for (var i = 0; i < main[1].speed.length; i++) {
+				if (i == main[1].speedIndex) {
+					speed += "[" + main[1].speed[i] + "] ";
+				} else {
+					speed += main[1].speed[i] + " ";
+				}
+				if (i == main[1].mightIndex) {
+					might += "[" + main[1].might[i] + "] ";
+				} else {
+					might += main[1].might[i] + " ";
+				}
+				if (i == main[1].sanityIndex) {
+					sanity += "[" + main[1].sanity[i] + "] ";
+				} else {
+					sanity += main[1].sanity[i] + " ";
+				}
+				if (i == main[1].knowledgeIndex) {
+					knowledge += "[" + main[1].knowledge[i] + "] ";
+				} else {
+					knowledge += main[1].knowledge[i] + " ";
+				}
+			}
+			actions.textContent += speed + "\r\n" + might + "\r\n" + sanity + "\r\n" + knowledge + "\r\n";
+		});
+		actions.appendChild(btn);
+	}
+	console.log(getTile("upper"));
 }
 
 // Gets random tile from stack, given current floor
